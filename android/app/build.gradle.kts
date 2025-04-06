a30b10d561df5770d63fb7aa8ea7eba3d0628ada
audiosiraj <sirajaudio0786@gmail.com>
2025-04-06 15:13:49 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.voice_to_text_app"
+    namespace = "com.example.faed_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
