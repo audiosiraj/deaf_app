b0b6651985b35bcfb8d7c58597f4988744e1c6ca
audiosiraj <sirajaudio0786@gmail.com>
2025-04-06 13:54:48 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:deafapp/main.dart';
+import 'package:voice_to_text_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(SpeakerDiarizationApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
